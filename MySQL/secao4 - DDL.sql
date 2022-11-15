c53732fe11d7c43bff999be18399cff34f917417
Fábio <fa2bio.s0@gmail.com>
2022-11-15 10:51:52 -0300

    Update secao4 - DDL.sql

--- MySQL/secao4 - DDL.sql
+++ MySQL/secao4 - DDL.sql
@@ -20,6 +20,41 @@ CREATE TABLE produtos(
 ALTER TABLE tipos_produtos ADD peso DECIMAL(8,2);
 ALTER TABLE tipos_produtos DROP peso;
 
-DROP TABLE tipos_produtos;
 DROP TABLE produtos;
+DROP TABLE tipos_produtos;
 DROP SCHEMA secao04;
+
+CREATE DATABASE juncao;
+
+USE juncao;
+
+CREATE TABLE profissoes(
+	id INT NOT NULL AUTO_INCREMENT,
+    cargo VARCHAR(60) NOT NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE clientes(
+	id INT NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(60) NOT NULL,
+    data_nascimento DATE NOT NULL,
+    telefone VARCHAR(10) NOT NULL,
+	id_profissao INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_profissao) REFERENCES profissoes(id)
+);
+
+CREATE TABLE consumidor(
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50) NOT NULL,
+    contato VARCHAR(50) NOT NULL,
+    endereco VARCHAR(100) NOT NULL,
+	cidade VARCHAR(100) NOT NULL,
+    cep VARCHAR(20) NOT NULL,
+    pais VARCHAR(30) NOT NULL
+);
+
+DROP TABLE profissoes;
+DROP TABLE clientes;
+DROP TABLE consumidores;
+DROP DATABASE juncao;
