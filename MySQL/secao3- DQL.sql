5c595f0a5afb28673559156921f8f0714126cb33
Fábio <fa2bio.s0@gmail.com>
2022-11-15 10:51:13 -0300

    Update secao3 - DQL.sql

--- MySQL/secao3- DQL.sql
+++ MySQL/secao3- DQL.sql
@@ -4,4 +4,9 @@ SELECT codigo, descricao FROM tipos_produtos;
 SELECT * FROM produtos;
 SELECT codigo, descricao, codigo_tipo FROM produtos;
 
+SELECT * FROM produtos WHERE codigo_tipo = 2;
+SELECT * FROM produtos WHERE codigo_tipo = 2 AND preco < 700;
 SELECT p.codigo AS cod, p.descricao AS descr, p.preco AS pre, p.codigo_tipo AS ctp FROM produtos AS p;
+
+# Joins
+SELECT * FROM clientes;
