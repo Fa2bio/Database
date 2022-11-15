7f158a7f6be8332e0d92dba1d81e139ea3fa4956
Fábio <fa2bio.s0@gmail.com>
2022-11-15 10:51:30 -0300

    Create secao3 - DTL.sql

---
+++ MySQL/secao4 - DTL.sql
@@ -0,0 +1,9 @@
+START TRANSACTION;
+	INSERT INTO tipos_produtos (descricao) VALUES ('Acessorios');
+    INSERT INTO tipos_produtos (descricao) VALUES ('Equipamentos');
+COMMIT;
+
+START TRANSACTION;
+	INSERT INTO tipos_produtos (descricao) VALUES ('Veículos');
+    INSERT INTO tipos_produtos (descricao) VALUES ('Aeronaves');
+ROLLBACK;
